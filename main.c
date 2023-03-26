13f168303ccd52186e2f3815c61a0bedbdbe8e27
George Davidson <gd240673@gmail.com>
2023-03-26 09:36:01 -0400

    update

--- main.c
+++ main.c
@@ -7,27 +7,26 @@
 #define MAX_SIZE 256
 
 char stack[MAX_SIZE];
-int top = 0;
+int top = -1;
 
-bool isFull() { return top == MAX_SIZE; }
+bool isFull() { return top == MAX_SIZE - 1; }
 
 bool isEmpty() { return top == -1; }
 
 void push(char x) {
   if (isFull()) {
-    printf("there is not space left in the stack\n");
+    printf("the stack is full\n");
     exit(1);
   }
-  stack[top++] = x;
+  stack[++top] = x;
 }
 
 char pop() {
   if (isEmpty()) {
-    printf("nothing to take from the stack\n");
+    printf("the stack is empty\n");
     exit(1);
   }
-  char result = stack[--top];
-  return result;
+  return stack[top--];
 }
 
 struct Node {
@@ -56,41 +55,37 @@ int precedence(char operator) {
     return 1;
   else if (operator== '*' || operator== '/')
     return 2;
-  else if (operator== '^')
-    return 3;
   else
     return 0;
 }
 
 void infixToPostfix(struct Node *head, char string[]) {
-  struct Node *current = head;
   int i = 0;
-  while (current != NULL) {
-    if (isalnum(*current->data) ||
-        current->data[0] == '-' && strlen(current->data) != 1) {
-      for (int j = 0; j < strlen(current->data); j++) {
-        string[i++] = current->data[j];
+  while (head != NULL) {
+    if (isalnum(*head->data) ||
+        (*head->data == '-' && strlen(head->data) > 1)) {
+      for (int j = 0; j < strlen(head->data); j++) {
+        string[i++] = head->data[j];
       }
       string[i++] = ' ';
-    } else if (*current->data == '(') {
-      push(*current->data);
-    } else if (*current->data == ')') {
-      while (stack[top - 1] != '(') {
+    } else if (*head->data == '(') {
+      push(*head->data);
+    } else if (*head->data == ')') {
+      while (stack[top] != '(') {
         string[i++] = pop();
         string[i++] = ' ';
       }
       pop();
     } else {
-      while (top != 0 &&
-             precedence(*current->data) <= precedence(stack[top - 1])) {
+      while (!isEmpty() && precedence(*head->data) <= precedence(stack[top])) {
         string[i++] = pop();
         string[i++] = ' ';
       }
-      push(*current->data);
+      push(*head->data);
     }
-    current = current->next;
+    head = head->next;
   }
-  while (top != 0) {
+  while (!isEmpty()) {
     string[i++] = pop();
     string[i++] = ' ';
   }
@@ -101,9 +96,9 @@ int main() {
   struct Node *head = NULL;
   char input[MAX_SIZE];
   char output[MAX_SIZE];
-  char tempString[16];
+  char tempString[MAX_SIZE];
 
-  printf("input: ");
+  printf("Input: ");
   fgets(input, MAX_SIZE, stdin);
 
   int i = 0;
@@ -121,6 +116,6 @@ int main() {
 
   infixToPostfix(head, output);
 
-  printf("output: %s\n", output);
+  printf("Output: %s\n", output);
   return 0;
 }
