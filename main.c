6862605d3b891a54e83366076b11bc465d437569
George Davidson <gd240673@gmail.com>
2023-06-26 14:10:58 -0400

    update

--- main.c
+++ main.c
@@ -9,49 +9,66 @@
 char stack[MAX_SIZE];
 int top = -1;
 
-bool isFull() { return top == MAX_SIZE - 1; }
+bool isFull()
+{
+    return top == MAX_SIZE - 1;
+}
 
-bool isEmpty() { return top == -1; }
+bool isEmpty()
+{
+    return top == -1;
+}
 
-void push(char x) {
-    if (isFull()) {
+void push(char x)
+{
+    if (isFull())
+    {
         printf("the stack is full\n");
         exit(1);
     }
     stack[++top] = x;
 }
 
-char pop() {
-    if (isEmpty()) {
+char pop()
+{
+    if (isEmpty())
+    {
         printf("the stack is empty\n");
         exit(1);
     }
     return stack[top--];
 }
 
-typedef struct node {
+typedef struct node
+{
     char *data;
     struct node *next;
 } node;
 
-void addNode(node **head, char *data) {
+void addNode(node **head, char *data)
+{
     node *newNode = malloc(sizeof(node));
     newNode->data = malloc(strlen(data));
     strcpy(newNode->data, data);
     newNode->next = NULL;
 
-    if (*head == NULL) {
+    if (*head == NULL)
+    {
         *head = newNode;
-    } else {
+    }
+    else
+    {
         node *current = *head;
-        while (current->next != NULL) {
+        while (current->next != NULL)
+        {
             current = current->next;
         }
         current->next = newNode;
     }
 }
 
-int precedence(char operator) {
+int precedence(char operator)
+{
     if (operator== '+' || operator== '-')
         return 1;
     else if (operator== '*' || operator== '/')
@@ -60,24 +77,38 @@ int precedence(char operator) {
         return 0;
 }
 
-void infixToPostfix(node *head, char string[]) {
+void infixToPostfix(node *head, char string[])
+{
     int i = 0;
-    while (head != NULL) {
-        if (isalnum(*head->data) || (*head->data == '-' && strlen(head->data) > 1)) {
-            for (int j = 0; j < strlen(head->data); j++) {
+    while (head != NULL)
+    {
+        if (isalnum(*head->data) || (*head->data == '-' &&
+                                     strlen(head->data) > 1))
+        {
+            for (int j = 0; j < strlen(head->data); j++)
+            {
                 string[i++] = head->data[j];
             }
             string[i++] = ' ';
-        } else if (*head->data == '(') {
+        }
+        else if (*head->data == '(')
+        {
             push(*head->data);
-        } else if (*head->data == ')') {
-            while (stack[top] != '(') {
+        }
+        else if (*head->data == ')')
+        {
+            while (stack[top] != '(')
+            {
                 string[i++] = pop();
                 string[i++] = ' ';
             }
             pop();
-        } else {
-            while (!isEmpty() && precedence(*head->data) <= precedence(stack[top])) {
+        }
+        else
+        {
+            while (!isEmpty() && precedence(*head->data) <=
+                                     precedence(stack[top]))
+            {
                 string[i++] = pop();
                 string[i++] = ' ';
             }
@@ -85,14 +116,16 @@ void infixToPostfix(node *head, char string[]) {
         }
         head = head->next;
     }
-    while (!isEmpty()) {
+    while (!isEmpty())
+    {
         string[i++] = pop();
         string[i++] = ' ';
     }
     string[i] = '\0';
 }
 
-int main() {
+int main()
+{
     node *head = NULL;
     char input[MAX_SIZE];
     char output[MAX_SIZE];
@@ -103,19 +136,22 @@ int main() {
 
     int i = 0;
     int j = 0;
-    while (input[i] != '\0') {
-        if (isspace(input[i])) {
+    while (input[i] != '\0')
+    {
+        if (isspace(input[i]))
+        {
             tempString[j++] = '\0';
             addNode(&head, tempString);
             j = 0;
-        } else {
+        }
+        else
+        {
             tempString[j++] = input[i];
         }
         i++;
     }
 
     infixToPostfix(head, output);
-
     printf("Output: %s\n", output);
     return 0;
 }
